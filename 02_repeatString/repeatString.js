28be44c2cb41d790bd619ca77035d7128602347b
Hayden Dingman <haydendingman@gmail.com>
2024-08-29 15:48:00 -0700

    Solve repeat string problem 02_repeatString for TOP

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let fullString = "";
+    for (let counter = 0; counter < num; counter++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
