1bb7ae0a9b9312dcdd1ac4bd8ca0b78385d88a75
Hayden Dingman <haydendingman@gmail.com>
2024-08-31 14:28:58 -0700

    Add function to calculate leap years for 06_leapYears for TOP

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 != 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// years % 4 = 0
+// years % 400 = 0
+// years % 100 != 0
