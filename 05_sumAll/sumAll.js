7e1fdfe843e46aa294a1f1123e291b2f049af6b4
Hayden Dingman <haydendingman@gmail.com>
2024-08-31 14:21:06 -0700

    Add ugly JS for sum between two numbers in 05_sumAll for TOP

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+    let sum = 0;
+    if (firstNumber > 0 && secondNumber > 0 && Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+        if (firstNumber > secondNumber) {
+            let swapValue = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = swapValue;
+        }
+        for (let i = firstNumber; i <= secondNumber; i++) {
+            sum += i;
+        }
+    } else {
+        return "ERROR";
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
