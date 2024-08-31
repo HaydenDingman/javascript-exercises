48c21f09b12f88790199263680c5864d1bcee4c0
Hayden Dingman <haydendingman@gmail.com>
2024-08-31 14:07:08 -0700

    Add JS to remove array arguments in 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...argsToRemove) {
+    let newArray = [];
+    outer: for (let counter = 0; counter < givenArray.length; counter++) {
+        for (const argument of argsToRemove) {
+            console.log(argument);
+            if (givenArray.at(counter) === argument){
+                continue outer;
+            }
+        }
+        newArray.push(givenArray.at(counter));
+    }
+    return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
