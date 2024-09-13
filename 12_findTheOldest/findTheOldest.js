4bebb03157dd109d270687e0f7757a0415bc7fac
Hayden Dingman <haydendingman@gmail.com>
2024-09-13 14:42:57 -0700

    Solve 12_findTheOldest for TOP

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array
+            .sort((a, b) => {
+                return (getAge(a.yearOfBirth, a.yearOfDeath)) - (getAge(b.yearOfBirth, b.yearOfDeath))
+            })
+            .pop()
+    
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
