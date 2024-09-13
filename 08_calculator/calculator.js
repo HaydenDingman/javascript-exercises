c7bf78c546b978890fd0608c70adb96a460a0aaf
Hayden Dingman <haydendingman@gmail.com>
2024-09-13 13:53:00 -0700

    Solve 08_calculator problem for TOP

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, current) => acc + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+  let result = 1;
+  for (let i = 0; i < b; i++) {
+      result *= a;
+  };
+  return result;
+}
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
