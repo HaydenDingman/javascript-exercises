6f45ebdb6cd2ab05f6e041fc9235616ec8008186
Hayden Dingman <haydendingman@gmail.com>
2024-09-13 14:17:53 -0700

    Solve 10_fibonnaci for TOP

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (+number === 0) return 0;
+    if (+number < 0) return "OOPS";
+    let lastNumber = 1;
+    let currentNumber = 1;
+    for (let i = 1; i < +number; i++) {
+        let temp = lastNumber;
+        lastNumber = currentNumber;
+        currentNumber = temp + currentNumber;
+    }
+    return lastNumber;
 };
 
 // Do not edit below this line
