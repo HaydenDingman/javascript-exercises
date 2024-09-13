29ca5385ba80955d2db3b016577f1e384f7e867e
Hayden Dingman <haydendingman@gmail.com>
2024-09-13 14:08:39 -0700

    Solve 09_palindromes for TOP

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const cleanedString = string
+                            .toLowerCase()
+                            .split('')
+                            .filter((char) => alphanumeric.includes(char))
+                            .join('');
 
+    let reversedString = cleanedString
+                                    .split('')
+                                    .reverse()
+                                    .join('');
+    if (cleanedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
